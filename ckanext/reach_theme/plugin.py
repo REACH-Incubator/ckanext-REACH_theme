f5bcc2abb3592a3bcf3ac8bab00805020b1ee757
Mikel Emaldi <m.emaldi@gmail.com>
2020-11-13 08:57:34 +0100

    Fixing a bug

--- ckanext/reach_theme/plugin.py
+++ ckanext/reach_theme/plugin.py
@@ -4,12 +4,12 @@ import ckan.plugins.toolkit as toolkit
 
 class Reach_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        plugins.implements(plugins.IRoutes, inherit=True)
+        toolkit.add_template_directory(config_, 'templates')  
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'reach_theme')
 
